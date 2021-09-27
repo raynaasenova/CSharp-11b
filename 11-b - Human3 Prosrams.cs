d6e6243b1674bb585c7df7bd47ac7c51157d3b9e
Rayna Asenova <rayna.asenova@gmail.com>
2021-09-27 22:09:41 +0300

    Update 11-b - Human3 Prosrams.cs

--- 11-b - Human3 Prosrams.cs
+++ 11-b - Human3 Prosrams.cs
@@ -7,12 +7,12 @@ namespace Human3
         static void Main(string[] args)
         {
             Human person1 = new Human(); //не се различават по нищо
-            Human person2 = new Human(); //докато ни се препълни рам паметта
+            Human person2 = new Human(); 
 
             person1.Name = "Stoyan"; //Даваме характеристики на човека
             person1.Age = 25;
 
-            person2.Name = "Ivan"; //Даваме характеристики на човека
+            person2.Name = "Ivan";
             person2.Age = 20;
 
             Console.WriteLine(person1.Name + ": " + person1.Age);
